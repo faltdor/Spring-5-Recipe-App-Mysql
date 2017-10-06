b85bc23399fc5977eb440bc5342cde7b8997d32d
faltdor <faltdor@gmail.com>
2017-10-05 23:51:45 -0400

    Add unit test for Sprint Mvc Test for Indexcontroller

--- src/test/java/com/faltdor/recipe/controllers/IndexControllerTest.java
+++ src/test/java/com/faltdor/recipe/controllers/IndexControllerTest.java
@@ -3,6 +3,9 @@ package com.faltdor.recipe.controllers;
 import static org.junit.Assert.*;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
 import java.util.HashSet;
 import java.util.Set;
@@ -12,6 +15,8 @@ import org.junit.Test;
 import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.ui.Model;
 
 import com.faltdor.recipe.controller.IndexController;
@@ -60,4 +65,12 @@ public class IndexControllerTest {
 		
 	}
 
+	@Test
+	public void testMockMVC() throws Exception {
+		MockMvc mockMvc = MockMvcBuilders.standaloneSetup(controller).build();
+		mockMvc.perform(get("/"))
+			   .andExpect(status().isOk())
+			   .andExpect(view().name("index"));
+			
+	}
 }
