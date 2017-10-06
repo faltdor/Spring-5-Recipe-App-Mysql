9e78f10f70f9230955da304a9fe134ad9e0229bf
faltdor <faltdor@gmail.com>
2017-10-05 23:41:21 -0400

    Refactor IndexControllerTest add given ,when and then

--- src/test/java/com/faltdor/recipe/controllers/IndexControllerTest.java
+++ src/test/java/com/faltdor/recipe/controllers/IndexControllerTest.java
@@ -4,13 +4,18 @@ import static org.junit.Assert.*;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.springframework.ui.Model;
 
 import com.faltdor.recipe.controller.IndexController;
+import com.faltdor.recipe.domain.Recipe;
 import com.faltdor.recipe.services.impl.RecipeServiceImpl;
 import static org.mockito.Mockito.*;
 
@@ -33,12 +38,25 @@ public class IndexControllerTest {
 	@Test
 	public void testGetIndexPage() {
 		
+		//Given
+		Set<Recipe> recipes = new HashSet<>();
+		recipes.add(new Recipe());
+		Recipe secondSecipe = new Recipe();
+		secondSecipe.setId(4L);
+		recipes.add(secondSecipe);
+		
+		//When
+		when(recipeService.getRecipes()).thenReturn(recipes);
+		ArgumentCaptor<Set<Recipe>> argumentCaptor = ArgumentCaptor.forClass(Set.class);
+		
 		String viewName = controller.getIndexPage(model);
 		
-		assertEquals("index", viewName);
-		
+		//Then
+		assertEquals("index", viewName);		
 		verify(recipeService,times(1)).getRecipes();
-		verify(model, times(1)).addAttribute(eq("recipes"), anySet());
+		verify(model, times(1)).addAttribute(eq("recipes"), argumentCaptor.capture());
+		Set<Recipe> setInController = argumentCaptor.getValue();
+		assertEquals(2, setInController.size());
 		
 	}
 
