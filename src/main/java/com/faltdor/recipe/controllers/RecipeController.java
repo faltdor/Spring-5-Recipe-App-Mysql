0058d136fdbd01fee08a002962170302abb74832
faltdor <faltdor@gmail.com>
2017-10-09 19:39:07 -0400

    iAssignment Enumeration Dropdowns with Spring MVC and Thymeleaf

--- src/main/java/com/faltdor/recipe/controllers/RecipeController.java
+++ src/main/java/com/faltdor/recipe/controllers/RecipeController.java
@@ -2,6 +2,7 @@ package com.faltdor.recipe.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -19,14 +20,14 @@ public class RecipeController {
 		this.recipeService = recipeService;
 	}
 	
-	@RequestMapping("/recipe/{id}/show")
+	@GetMapping("/recipe/{id}/show")
 	public String showById(@PathVariable String id,Model model) {
 		model.addAttribute("recipe", recipeService.findById(new Long(id)));
 		
 		return "recipe/show";
 	}
 	
-	@RequestMapping("/recipe/new")
+	@GetMapping("/recipe/new")
 	public String newRecipe(Model model) {
 		
 		
@@ -35,15 +36,15 @@ public class RecipeController {
 		
 	}
 	
-	@RequestMapping("/recipe/{id}/update")
+	@GetMapping("/recipe/{id}/update")
 	public String updateRecipe(@PathVariable String id,Model model) {
 		model.addAttribute("recipe", recipeService.findCommandById(Long.valueOf(id)));
 		
 		return "recipe/recipeform";
 	}
 	
-	@PostMapping
-	@RequestMapping("/recipe")
+	
+	@PostMapping("/recipe")
 	public String saveOrUpdateRecipe(@ModelAttribute RecipeCommand command ) {
 		
 		RecipeCommand savedCommand = recipeService.saveRecipeCommand(command);
@@ -51,8 +52,7 @@ public class RecipeController {
 		return "redirect:/recipe/"+savedCommand.getId()+"/show";
 	}
 	
-	@PostMapping
-	@RequestMapping("/recipe/{id}/delete")
+	@GetMapping("/recipe/{id}/delete")
 	public String deleteRecipe(@PathVariable String id ) {
 		
 		recipeService.deleteById(Long.valueOf(id));
